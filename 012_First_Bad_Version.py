3d689f9dc4a55e18ba56079e21a1a16a2860f557
MaryamMaksour <118085088+MaryamMaksour@users.noreply.github.com>
2025-09-11 10:35:16 +0400

    Update 012_First_Bad_Version.py

--- 012_First_Bad_Version.py
+++ 012_First_Bad_Version.py
@@ -1,15 +1,35 @@
+'''
+
+Linke:
+https://leetcode.com/problems/first-bad-version/
+
+
+Runtime 
+36 ms, Beats 66.64%
+
+Memory 
+17.79 MB, Beats 43.75%
+
+'''
 # The isBadVersion API is already defined for you.
 # def isBadVersion(version: int) -> bool:
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        low=1
-        high=n
-        while low<high:
-            mid=low+(high-low)//2
-            if isBadVersion(mid)==True:
-                high=mid
-            elif isBadVersion(mid)==False:
-                low=mid+1
-        return low
- 
+
+        l = -1
+        r = n+1
+        ans = 0
+
+        while l + 1 < r:
+            
+            mid = l + int((r - l)/2)
+
+            if ( isBadVersion(mid)):
+                r = mid
+                ans = mid
+            else:
+                l = mid
+
+        return ans
+        
